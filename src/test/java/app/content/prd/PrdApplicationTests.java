13ab8ccc1ddf1e61f319eb82989fa6646fa6e0bc
Alicja Wiercioch <homazmora@gmail.com>
2024-02-05 09:51:10 +0100

    init

---
+++ src/test/java/app/content/prd/PrdApplicationTests.java
@@ -0,0 +1,13 @@
+package app.content.prd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
